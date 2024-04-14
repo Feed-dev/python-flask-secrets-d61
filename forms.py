7f5b98e88e6e7f0d7751581cc5c4aa13ea0bff00
federico Lubbert <federico.lubbert@gmail.com>
2024-04-14 15:03:54 +0200

    init

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()], render_kw={"size": "30"})
-    password = PasswordField(label='Password', validators=[DataRequired()], render_kw={"size": "30"})
-    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from flask import Flask, render_template, redirect, url_for
-from forms import LoginForm
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length  # pip install email-validator
+from flask_bootstrap import Bootstrap5  # pip install bootstrap-flask
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
+
 
 app = Flask(__name__)
-app.secret_key = "some secret string"
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+bootstrap = Bootstrap5(app)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
     return render_template('index.html')
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect(url_for('home'))
-    return render_template("login.html", form=form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
